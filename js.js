226c48e75e38be363fdad859798900df01d519bb
Max Gagner <m.gagner@hotmail.se>
2024-10-24 23:48:11 +0200

    started with login screen and css improvements

---
+++ js.js
@@ -0,0 +1,12 @@
+var login = false;
+window.onload = async function() {
+    /*
+    if(document.getElementById("loginForm")) {
+        login = true;
+    }
+    if (!login) {
+        window.location = "login.html";
+        login = true;
+    }
+    */
+}
