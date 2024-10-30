e492c07f18046691dd8bd607ab415a3098291f4a
Max Gagner <m.gagner@hotmail.se>
2024-10-30 19:48:51 +0100

    new css for the added parts and minor tweaks

--- js.js
+++ js.js
@@ -1,7 +1,6 @@
 //local url
 let url = "http://127.0.0.1:3000";
 const { Buffer } = require('node:buffer');
-const { log } = require('node:console');
 
 var login = false;
 window.onload = async function() {
@@ -31,6 +30,14 @@ window.onload = async function() {
         document.getElementById("createButton").addEventListener("click", async ()=> {await createGroup(document.getElementById("joinForm"))});
     }
 
+    if(document.getElementById("messageForm")) {
+        document.getElementById("messageForm").addEventListener("submit", async ()=> {await uploadMessage(document.getElementById("messageForm"))});
+    }
+
+    if(document.getElementById("fileUpload")) {
+        document.getElementById("fileUpload").addEventListener("submit", async ()=> {await uploadFile(document.getElementById("fileUpload"))});
+    }
+
     if (document.getElementById("options")) {
         let members = document.getElementById("members");
         let message = document.getElementById("message");
@@ -42,28 +49,52 @@ window.onload = async function() {
             message.style.display = "block";
             getFile.style.display = "none";
             enterFile.style.display = "none";
+            localStorage.setItem("selected", 0)
         })
         document.getElementById("optionTable").children[1].addEventListener("click", () => {
             members.style.display = "none";
             message.style.display = "none";
             getFile.style.display = "block";
             enterFile.style.display = "none";
+            localStorage.setItem("selected", 1)
         })
         document.getElementById("optionTable").children[2].addEventListener("click", () => {
             members.style.display = "none";
             message.style.display = "none";
             getFile.style.display = "none";
             enterFile.style.display = "block";
+            localStorage.setItem("selected", 2)
         })
         document.getElementById("optionTable").children[3].addEventListener("click", () => {
             members.style.display = "block";
             message.style.display = "none";
             getFile.style.display = "none";
             enterFile.style.display = "none";
+            localStorage.setItem("selected", 3)
         })
+        const event = new Event("click");
+        switch(localStorage.getItem("selected")) {
+            case "0":
+                console.log("ran");
+                document.getElementById("optionTable").children[0].dispatchEvent(event);
+                break;
+            case "1":
+                document.getElementById("optionTable").children[1].dispatchEvent(event);
+                break;
+            case "2":
+                document.getElementById("optionTable").children[2].dispatchEvent(event);
+                break;
+            case "3":
+                document.getElementById("optionTable").children[3].dispatchEvent(event);
+                break;
+        }
+
+        if (localStorage.getItem("lastGroup")) {
+            let oldGroup = JSON.parse(localStorage.getItem("lastGroup"));
+            console.log(oldGroup);
+            selectGroup(oldGroup.username, oldGroup.groupname);
+        }
     }
-
-
 
     if(!document.getElementById("loginForm") && !login) {
         window.location = "login.html";
@@ -130,18 +161,20 @@ async function populate() {
         let container = document.getElementById("groupList")
         for (let index = 0; index < login.member.length; index++) {
             let div = document.createElement("div");
+            let biggerDiv = document.createElement("div");
             let p = document.createElement("h2");
             let button = document.createElement("button");
-            button.innerHTML = "leave group";
+            button.innerHTML = "Leave Group";
             button.addEventListener("click", async ()=> {await leaveGroup(login.username, login.member[index])});
             let button2 = document.createElement("button");
-            button2.innerHTML = "select group";
+            button2.innerHTML = "Select Group";
             button2.addEventListener("click", async ()=> {await selectGroup(login.username, login.member[index])});
             p.innerHTML = login.member[index];
             div.append(button);
             div.append(button2);
-            container.append(p);
-            container.append(div);
+            biggerDiv.append(p);
+            biggerDiv.append(div);
+            container.append(biggerDiv);
         }
     }
 }
@@ -262,7 +295,69 @@ async function uploadProfileImg(img) {
     }
 }
 
+async function uploadFile(newFile) {
+    let file = newFile[0].files[0];
+
+    let usableFile;
+    try {
+        usableFile = await readFile(file);   
+    } catch (error) {
+        console.log("invalid file, ignoring for serverside error");
+        let errorHandler = document.getElementById("fileUploadError");
+        errorHandler.innerHTML = "Invalid file, please try again";
+        errorHandler.style.display = "block";
+        return
+    }
+
+    if(file.size > 16000000) {
+        let errorHandler = document.getElementById("fileUploadError");
+        errorHandler.innerHTML = "File to large, please choose a file under 16MB";
+        errorHandler.style.display = "block";
+        return;
+    }
+
+    console.log(file)
+
+    let user = JSON.parse(localStorage.getItem("loginData"));
+    let res = await fetch(url + "/groupUploadFile", {
+        method: 'PUT',
+        body: JSON.stringify({username: user.username, groupname: document.getElementById("groupname").innerHTML, file: usableFile, fileName: file.name}),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => response.json())
+    if(res.error) {
+        let errorHandler = document.getElementById("fileUploadError");
+        errorHandler.innerHTML = res.error;
+        errorHandler.style.display = "block";
+    }
+    else if(res.message){
+        await reLog(user.username);
+    }
+}
+
+async function uploadMessage(message) {
+    let login = JSON.parse(localStorage.getItem("loginData"));
+    console.log(document.getElementById("groupname").innerHTML);
+    let res = await fetch(url + "/groupMessage", {
+        method: 'POST',
+        body: JSON.stringify({username: login.username, groupname: document.getElementById("groupname").innerHTML, message: message[0].value}),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => response.json())
+    if(res.error) {
+        let errorHandler = document.getElementById("messageError");
+        errorHandler.innerHTML = res.error;
+        errorHandler.style.display = "block";
+    }
+    else if(res.message){
+        await reLog(login.username);
+    }
+}
+
 async function selectGroup(username, groupname) {  
+    localStorage.setItem("lastGroup", JSON.stringify({username, groupname}));
     let res = await fetch(url + "/groupFetch", {
         method: 'POST',
         body: JSON.stringify({username: username, groupname: groupname}),
@@ -274,7 +369,7 @@ async function selectGroup(username, groupname) {
         console.log("something went wrong");
     }
     let members = document.getElementById("members")
-    let messageList = document.getElementById("message")
+    let messageList = document.getElementById("messageBox")
     let getFile = document.getElementById("getFile")
 
     document.getElementById("groupname").innerHTML = res.groupFile.name;
@@ -285,11 +380,13 @@ async function selectGroup(username, groupname) {
         members.append(p);
     }
 
+
     for (let index = 0; index < res.groupFile.messages.length; index++) {
         let container = document.createElement("div");
         let user = document.createElement("h5");
         let message = document.createElement("p");
-        user.innerHTML = res.groupFile.messages[index].user + " - " + res.groupFile.messages[index].time;
+        let date = new Date(res.groupFile.messages[index].time);
+        user.innerHTML = res.groupFile.messages[index].user + " - " + date.getHours() + ":" + date.getMinutes() + " " + date.getDate() +"/"+ date.getMonth();
         message.innerHTML = res.groupFile.messages[index].message;
         container.append(user);
         container.append(message);
@@ -297,15 +394,22 @@ async function selectGroup(username, groupname) {
     }
 
     for (let index = 0; index < res.groupFile.files.length; index++) {
+        console.log(res.groupFile);
         let container = document.createElement("div");
         let user = document.createElement("h5");
         let file = document.createElement("a");
-        user.innerHTML = res.groupFile.files[index].user + " - " + res.groupFile.files[index].time;
+        let date = new Date(res.groupFile.messages[index].time);
+        user.innerHTML = res.groupFile.files[index].user + " - " + date.getHours() + ":" + date.getMinutes() + " " + date.getDate() +"/"+ date.getMonth() + "/" + date.getFullYear();
         file.innerHTML = res.groupFile.files[index].name;
-        file.setAttribute("href", res.groupFile.files[index].file);
-        file.setAttribute("download")
+        file.setAttribute("download", res.groupFile.files[index].file);
+
+        let buff = Buffer.from(res.groupFile.files[index].file);
+        let refinedFile = buff.toString('base64');
+        file.setAttribute("href", atob(refinedFile));
+        file.setAttribute("download", res.groupFile.files[index].name);
+
         container.append(user);
         container.append(file);
-        messageList.append(container);
+        getFile.append(container);
     }
 }
